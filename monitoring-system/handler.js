8383c5d2f370814116f7a883126cc7f0e7164db8
dohyeon5626 <kodohyeon71@gmail.com>
2025-03-29 10:21:04 +0900

    Add github html extension add button selector check logic
    #2

--- monitoring-system/handler.js
+++ monitoring-system/handler.js
@@ -2,10 +2,25 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 exports.run = async () => {
-    console.log(await getGithubHtmlExtensionUsercount())
+  const [
+    githubHtmlExtensionUsercount,
+    githubHtmlExtensionAddButtonSelectorExistence
+  ] = await Promise.all([
+    getGithubHtmlExtensionUsercount(),
+    hasGithubHtmlExtensionAddButtonSelector()
+  ]);
+  console.log(githubHtmlExtensionUsercount)
+  console.log(githubHtmlExtensionAddButtonSelectorExistence)
 };
 
+/* Github Html Preview Extension 사용자 수 */
 getGithubHtmlExtensionUsercount = async () => {
-    const { data } = await axios.get("https://chromewebstore.google.com/detail/github-html-preview/pmpjligbgooljdpakhophgddmcipglna");
-    return cheerio.load(data)('.F9iKBc').contents().eq(2).text().trim().split(" ")[0];
+  const { data } = await axios.get("https://chromewebstore.google.com/detail/github-html-preview/pmpjligbgooljdpakhophgddmcipglna");
+  return cheerio.load(data)('.F9iKBc').contents().eq(2).text().trim().split(" ")[0];
+}
+
+/* Github Html Preview Extension 버튼 생성 오류 여부 */
+hasGithubHtmlExtensionAddButtonSelector = async () => {
+  const { data } = await axios.get("https://github.com/dohyeon5626/github-html-preview-extension/blob/main/public/popup/popup.html");
+  return cheerio.load(data)('.Box-sc-g0xbh4-0 .kcLCKF div > a[data-testid=raw-button]').length == 1;
 }

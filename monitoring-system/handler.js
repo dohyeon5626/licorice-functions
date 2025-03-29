fc7299c3812afbd439247b1b7b59d4cbd4840dfb
dohyeon5626 <kodohyeon71@gmail.com>
2025-03-29 13:04:16 +0900

    Add auto gitkeep plugin download count get logic
    #2

--- monitoring-system/handler.js
+++ monitoring-system/handler.js
@@ -4,13 +4,16 @@ const cheerio = require("cheerio");
 exports.run = async () => {
   const [
     githubHtmlExtensionUsercount,
-    githubHtmlExtensionAddButtonSelectorExistence
+    githubHtmlExtensionAddButtonSelectorExistence,
+    autoGitkeepPluginDownloadcount
   ] = await Promise.all([
     getGithubHtmlExtensionUsercount(),
-    hasGithubHtmlExtensionAddButtonSelector()
+    hasGithubHtmlExtensionAddButtonSelector(),
+    getAutoGitkeepPluginDownloadcount()
   ]);
   console.log(githubHtmlExtensionUsercount)
   console.log(githubHtmlExtensionAddButtonSelectorExistence)
+  console.log(autoGitkeepPluginDownloadcount)
 };
 
 /* Github Html Preview Extension 사용자 수 */
@@ -23,4 +26,10 @@ getGithubHtmlExtensionUsercount = async () => {
 hasGithubHtmlExtensionAddButtonSelector = async () => {
   const { data } = await axios.get("https://github.com/dohyeon5626/github-html-preview-extension/blob/main/public/popup/popup.html");
   return cheerio.load(data)('.Box-sc-g0xbh4-0 .kcLCKF div > a[data-testid=raw-button]').length == 1;
+}
+
+/* Auto Gitkeep Plugin 다운로드 수 */
+getAutoGitkeepPluginDownloadcount = async () => {
+  const { data } = await axios.get("https://plugins.jetbrains.com/api/plugins/20950");
+  return data.downloads;
 }

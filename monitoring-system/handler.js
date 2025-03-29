7bbb94e7ecfe16a95e10424964ee79fc9432722b
dohyeon5626 <kodohyeon71@gmail.com>
2025-03-29 10:03:42 +0900

    Add github html extension user count get logic
    #2

--- monitoring-system/handler.js
+++ monitoring-system/handler.js
@@ -1,4 +1,11 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
 exports.run = async () => {
-  const time = new Date();
-  console.log(`Your cron function ran at ${time}`);
+    console.log(await getGithubHtmlExtensionUsercount())
 };
+
+getGithubHtmlExtensionUsercount = async () => {
+    const { data } = await axios.get("https://chromewebstore.google.com/detail/github-html-preview/pmpjligbgooljdpakhophgddmcipglna");
+    return cheerio.load(data)('.F9iKBc').contents().eq(2).text().trim().split(" ")[0];
+}
